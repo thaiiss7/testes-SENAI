a844a88da34a0b65d038a773384f278bf2043327
thaiiss7 <thaismichel7@gmail.com>
2026-05-22 10:34:38 -0300

    aula11 - pytest - assert

--- calculator.py
+++ calculator.py
@@ -34,3 +34,60 @@ def temperatureCtoF(c):
 
 def temperatureFtoC(f):
     return (f - 32) * 5/9
+
+# calculo de areas
+def squareArea(l):
+    return l * l
+
+def trianguleArea(b, a):
+    return b * a
+
+def elipseArea(a, b):
+    pi = 3.14
+    return pi * a * b
+
+# calculo de conversão
+def cmToM(a):
+    return a / 100
+
+def mToCm(a):
+    return a * 100
+
+def lToMl(a):
+    return a * 1000
+
+def mlToL(a):
+    return a / 1000
+
+def realToDolar(a):
+    return round(a / 5, 2)
+
+def dolarToReal(a):
+    return round(a * 5, 2)
+
+# calculo de salario
+def salary(v, h):
+    bruto = v * h
+
+    if (bruto <= 5000.00):
+        inss = bruto * 0.11 
+        ir = bruto * 0.00
+    else:
+        inss = bruto * 0.14
+        ir = bruto * 0.05
+
+    return bruto - inss - ir
+
+# autentificação
+def auth(u, p):
+    user = "thaiis"
+    password = 123
+
+    if (user == u and password == p):
+        return "login!"
+    elif (user != u and password != p):
+        return "tudo inválido!"
+    elif (user != u):
+        return "usuário inválido!"
+    elif (password != p):
+        return "senha inválida!"
--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,18 @@
 from calculator import add, subract, multiply, divide
 from calculator import minNumber, evenNumber, temperatureCtoF, temperatureFtoC
+from calculator import squareArea, trianguleArea, elipseArea
+from calculator import cmToM, mToCm, lToMl, mlToL, realToDolar, dolarToReal
+from calculator import salary
+from calculator import auth
 
-# def test_divide():
-#     assert divide(8,0) == print("it's not possible to divide by 0")
+def test_divide():
+    assert divide(8,0) == print("it's not possible to divide by 0")
 
-# def test_multiply():
-#     assert multiply(2,4) == 8
+def test_multiply():
+    assert multiply(2,4) == 8
 
-# def test_subtract():
-#     assert subract(10,5) == 5
+def test_subtract():
+    assert subract(10,5) == 5
 
 def test_minNumber():
     assert minNumber(5,7) == 5
@@ -19,5 +23,47 @@ def test_evenNumber():
 def test_celsius():
     assert temperatureCtoF(25) == 77.0
 
-# def test_fahrenheit():
-#     assert temperatureCtoF(68) == 20.0
+def test_fahrenheit():
+    assert temperatureFtoC(68) == 20.0
+
+def test_squareArea():
+    assert squareArea(4) == 16
+
+def test_trianguleArea():
+    assert trianguleArea(5, 2) == 10
+
+def test_elipseArea():
+    assert elipseArea(4, 2) == 25.12
+
+def test_cmToM():
+    assert cmToM(200) == 2
+
+def test_mToCm():
+    assert mToCm(1) == 100
+
+def test_lToMl():
+    assert lToMl(5) == 5000
+
+def test_mlToL():
+    assert mlToL(1000) == 1
+
+def test_realToDolar():
+    assert realToDolar(100) == 20.00
+
+def test_dolarToReal():
+    assert dolarToReal(100) == 500.00
+
+def test_salary():
+    assert salary(10, 300) == 2670.00
+
+def test_loginValido():
+    assert auth("thaiis", 123) == "login!"
+
+def test_usuarioInvalido():
+    assert auth("thais", 123) == "usuário inválido!"
+
+def test_senhaInvalida():
+    assert auth("thaiis", 124) == "senha inválida!"
+
+def test_loginInvalido():
+    assert auth("thaiss", 223) == "tudo inválido!"
